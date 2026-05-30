7aa6b67d2d68bc620585a0f0412d0d554566da9f
goldns2023-a11y <goldns2023@gmail.com>
2026-05-30 04:29:34 +0100

    Update upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -13,7 +13,6 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
-    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
       cb(null, `products/${Date.now()}-${file.originalname}`);
